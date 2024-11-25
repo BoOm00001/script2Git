ddc2e3ed635baf961976d01926936c883305ff6a
BoOm00001 <cherifouattara7@gmail.com>
2024-11-25 14:18:56 -0500

    Ajout de fonctionnalités pour la gestion des utilisateurs et configuration du dossier bienvenue
     Définir le chemin utilisateur pour le répertoire de bienvenue.
     Création du répertoire de bienvenue avec les permissions appropriées.
     Création et remplissage du fichier de bienvenue avec des informations utilisateur et poste de travail.
     Ajout de l'adresse IP au fichier de bienvenue, en excluant l'adresse locale (127.0.0.1).
     Message de confirmation pour indiquer la réussite de la configuration.
     Affichage du contenu du fichier de bienvenue pour validation.

--- Script2TP3.sh
+++ Script2TP3.sh
@@ -15,3 +15,25 @@ done
 # Crée un utilisateur sans mot de passe et affichage
 echo "$MDPADMIN" | sudo -S adduser --gecos "" --allow-bad-names --disabled-password --quiet "$NOM_UTILISATEUR"
 
+UTILISATEUR_HOME="/home/$NOM_UTILISATEUR/Bienvenue"
+
+
+sudo mkdir -p "$UTILISATEUR_HOME"
+sudo chown "$NOM_UTILISATEUR:$NOM_UTILISATEUR" "$UTILISATEUR_HOME"
+sudo chmod u+rwx "$UTILISATEUR_HOME"
+
+
+FICHIER_BIENVENUE="$UTILISATEUR_HOME/fichier.txt"
+
+sudo -u "$NOM_UTILISATEUR" bash -c "echo 'Bienvenue $NOM_UTILISATEUR sur votre nouveau poste de travail $NOM_POSTE_DE_T>
+sudo -u "$NOM_UTILISATEUR" bash -c "echo 'Votre répertoire de travail est : $UTILISATEUR_HOME (Droits : propriétaire un>
+
+
+sudo -u "$NOM_UTILISATEUR" bash -c "ip addr show | grep 'inet' | grep -v '127.0.0.1' | awk '{print \$2}' | cut -d/ -f1 >
+
+
+echo "Utilisateur $NOM_UTILISATEUR créé avec succès et répertoire de bienvenue configuré."
+
+
+echo "$MDPADMIN " | sudo cat "$FICHIER_BIENVENUE"
+
